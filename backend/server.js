618c6ff3e7dfa51e7bb88920ce50d15bf7c9f436
PawanK06 <pawan06kumar2003@gmail.com>
2024-02-16 00:37:46 +0530

    back to normal

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 import { postsRoutes } from "./routes/post.route.js";
 import { usersRoutes } from "./routes/user.route.js";
-import cors from 'cors';
-import 'dotenv/config.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import cors from 'cors'
+import 'dotenv/config.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', "https://mern-task-app-8nct.onrender.com"],
-  credentials: true
+  origin: ['http://localhost:5173', "https://mern-task-app-8nct.onrender.com"], // Specify the allowed origin
+  credentials: true // Allow credentials (cookies)
 }));
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
-
-// Serve static files from the 'frontend/build' directory
-app.use(express.static(join(__dirname, "frontend/dist")));
-
-// Serve the same HTML file for all routes
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "frontend/dist", "index.html"));
-});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
 
 mongoose
-  .connect(process.env.MONGODB_URI, { dbName: 'blogapp', useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, {dbName: 'blogapp'})
   .then(() => {
-    console.log("Connected to MongoDB successfully");
-    const port = process.env.PORT || 4000;
-    app.listen(port, () => console.log(`Server is running on port ${port}`));
+    console.log("connected to DB sucessfully");
+    app.listen(4000, () => console.log("Server is running on port 4000"));
   })
   .catch((err) => {
-    console.error("Failed to connect to MongoDB:", err);
+    console.log("Failed to connect MONGODB : ", err);
   });
