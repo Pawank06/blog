68221c88eb25505226c7ec9e887257cc1d79ffcb
PawanK06 <pawan06kumar2003@gmail.com>
2024-02-16 00:20:43 +0530

    go

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,12 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use(express.static(path.join(__dirname, "frontend/dist"))); // Adjust the path to match your frontend build output directory
+app.use(express.static(path.join(__dirname, "frontend/build"))); // Adjust the path to match your frontend build output directory
 
 // Your other route handlers and middleware
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html")); // Adjust the path to match your frontend build output directory
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html")); // Adjust the path to match your frontend build output directory
 });
 
 mongoose
